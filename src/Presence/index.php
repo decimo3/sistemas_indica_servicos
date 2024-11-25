4e246f2b20d36034313c601bb90e152292d00d8d
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-11-25 16:15:32 -0300

    Add iindex page to presence system

---
+++ src/Presence/index.php
@@ -0,0 +1,43 @@
+<?php
+  require_once(__DIR__ . "/../vendor/autoload.php");
+  use App\Core\Templates\Template;
+  $camera_icon_url = "\"" . __DIR__ . "Public/img/camera_button.png" . "\"";
+  new Template(
+    titulo: "Presença no DDS",
+    estilo: <<<CSS
+      #camera_button {
+        width: 150px;
+        height: 150px;
+        opacity: 0; /* Hide the default file input */
+        position: absolute;
+        z-index: 2; /* Place the input above */
+      }
+      #camera_button + label {
+        width: 150px;
+        height: 150px;
+        background: url($camera_icon_url) no-repeat center center;
+        background-size: cover;
+        display: inline-block;
+        cursor: pointer;
+        z-index: 1; /* Custom button below the input */
+      }
+    CSS,
+    conteudo: <<<HTML
+    <form id="formulario" action="./result.php" method="post" enctype="multipart/form-data" class="card border p-2">
+      <label for="matricula" class="mt-2">Matrícula Light:</label>
+      <br/>
+      <input type="number" class="form-control my-2" name="matricula" id="matricula" require>
+      <br/>
+      <span>Enviar foto:</span>
+      <div class="d-flex flex-row justify-content-around">
+        <input type="file" class="form-control" id="camera_button" name="camera_button" accept="image/*" capture="camera" require>
+        <label for="camera_button"></label>
+      </div>
+      <br/>
+      <input type="submit" class="btn btn-primary" value="Enviar imagem">
+      <br/>
+      <p><small>Camera icon from <a href="https://shmector.com/free-vector/technics/flat_camera_icon/12-0-1239" target="_blank">shmector.com</a></small></p>
+    </form>
+    HTML
+    );
+?>
