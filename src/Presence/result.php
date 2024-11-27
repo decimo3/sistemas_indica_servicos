15dfa246b757c654392346f6f0d2c2701c2d57d3
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-11-27 09:54:26 -0300

    Add signup users

--- src/Presence/result.php
+++ src/Presence/result.php
@@ -7,6 +7,7 @@
   require_once(__DIR__ . "/../vendor/autoload.php");
   use App\Core\Templates\Template;
   use App\Presence\Schemas\RequestPresenceList;
+  use App\Presence\Schemas\ResultPresenceList;
   use App\Presence\Services\Database;
   // Verifica se o arquivo foi postado
   if (!(is_uploaded_file($_FILES['camera_button']['tmp_name'])))
@@ -25,7 +26,6 @@
   }
   $presence_item = new RequestPresenceList($_POST["matricula"], $_FILES['camera_button']['tmp_name']);
   $presence_item->is_valid();
-  $database = new Database();
   $conteudo = "";
   if (!empty($presence_item->erros_validacao))
   {
@@ -46,18 +46,46 @@
     http_response_code(400);
     exit();
   }
-  new Template(
-    titulo: "Fuck! This have passed!",
-    estilo: "",
-    conteudo: <<<HTML
-      <p><a href="./">Voltar</a></p>
-    HTML
-    );
-  exit();
+  $database = new Database();
   $target_file = "/var/www/uploads/" . $presence_item->matricula . ".jpg";
   // Se o arquivo não existir, cadastrar usuário.
-  if (file_exists($target_file))
+  if(!file_exists($target_file))
   {
-    shell_exec("");
+    try
+    {
+      move_uploaded_file($presence_item->fotografia, $target_file);
+      $database->set_one_presence(
+        new ResultPresenceList(
+          rowid: 0,
+          matricula: $presence_item->matricula,
+          timestamp: new \DateTime(),
+          singularity: 1,
+          state: 201
+        )
+      );
+      new Template(
+        titulo: "Cadastro efetuado!",
+        estilo: "",
+        conteudo: <<<HTML
+          <p>Cadastro efetuado com sucesso!</p>
+          <p><a href="./">Voltar</a></p>
+        HTML
+        );
+      exit();
+    }
+    catch (\Throwable $th)
+    {
+      new Template(
+        titulo: "Algo deu errado!",
+        estilo: "",
+        conteudo: <<<HTML
+          <p class="text-danger">Houve um erro ao receber o arquivo.</p>
+          <p class="text-danger">$th->message</p>
+          <p>Sua presença <strong>não</strong> foi registrada!</p>
+          <p><a href="./">Voltar</a></p>
+        HTML
+        );
+      exit();
+    }
   }
 ?>
