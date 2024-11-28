2b592c0fea96eb5aa902b669f407b914757b8fc9
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-11-28 12:38:33 -0300

    Fix: changed type of property  erros_validacao from array to string and renamed

--- src/Presence/Schemas/RequestPresenceList.php
+++ src/Presence/Schemas/RequestPresenceList.php
@@ -4,34 +4,34 @@
   {
     public $matricula;
     public $fotografia;
-    public $erros_validacao;
+    public $erro_validacao;
     function __construct($matricula, $fotografia)
     {
       $this->matricula = $matricula;
       $this->fotografia = $fotografia;
-      $this->erros_validacao = [];
+      $this->erro_validacao = "";
     }
     function is_valid()
     {
       if (!(preg_match("/^[0-9]{7}$/", $this->matricula)))
       {
-        $this->erros_validacao[] = "A matrícula informada não é válida!";
+        $this->erro_validacao = "A matrícula informada não é válida!";
         return;
       }
       $image_properties = exif_read_data($this->fotografia);
       if (!($image_properties))
       {
-        $this->erros_validacao[] = "A imagem enviada não é válida!";
+        $this->erro_validacao = "A imagem enviada não é válida!";
         return;
       }
       if ($image_properties["FileType"] !== 2)
       {
-        $this->erros_validacao[] = "O tipo de imagem deve ser JPG ou JPEG!";
+        $this->erro_validacao = "O tipo de imagem deve ser JPG ou JPEG!";
         return;
       }
       if(!(isset($image_properties["DateTimeOriginal"])))
       {
-        $this->erros_validacao[] = "A imagem enviada é inválida!";
+        $this->erro_validacao = "A imagem enviada é inválida!";
         return;
       }
       $fotograph_timestamp = strtotime($image_properties["DateTimeOriginal"]);
@@ -39,13 +39,13 @@
       $difference_in_seconds = time() - $fotograph_timestamp;
       if($difference_in_seconds < 0 or $difference_in_seconds > 60)
       {
-        $this->erros_validacao[] = "A imagem enviada expirou!";
+        $this->erro_validacao = "A imagem enviada expirou!";
         return;
       }
       $modelos_homologados = ["moto e13", "etc.."];
       if (!(in_array($image_properties["Model"], $modelos_homologados)))
       {
-        $this->erros_validacao[] = "Câmera \"" . $image_properties["Model"] . "\" não homologada!";
+        $this->erro_validacao = "Câmera \"" . $image_properties["Model"] . "\" não homologada!";
         return;
       }
     }
