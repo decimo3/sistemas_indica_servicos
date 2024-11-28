18814d627c61d38bc695603ce97b798a10b36ebd
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-11-28 12:40:07 -0300

    Refactor page of presence list form result

--- src/Presence/result.php
+++ src/Presence/result.php
@@ -9,83 +9,50 @@
   use App\Presence\Schemas\RequestPresenceList;
   use App\Presence\Schemas\ResultPresenceList;
   use App\Presence\Services\Database;
-  // Verifica se o arquivo foi postado
-  if (!(is_uploaded_file($_FILES['camera_button']['tmp_name'])))
+  use App\Core\Services\FileService;
+  try {
+    // Verifica se o arquivo foi postado
+    if(!(is_uploaded_file($_FILES['camera_button']['tmp_name'])))
+    {
+      throw new \Exception("Houve um erro ao receber o arquivo.");
+    }
+    $presence_item = new RequestPresenceList($_POST["matricula"], $_FILES['camera_button']['tmp_name']);
+    $presence_item->is_valid();
+    if(!empty($presence_item->erro_validacao))
+    {
+      throw new \Exception($presence_item->erro_validacao);
+    }
+    $filehandler = new FileService();
+    $target_file = $presence_item->matricula . ".jpg";
+    if(!$filehandler->check_file_exist($target_file))
+    {
+      throw new \Exception("Funcionário não cadastrado!");
+    }
+    $download_file = $filehandler->download_file($target_file);
+    $result = shell_exec("presence_list.exe" . " " . "$target_file" . " " . "$dowload_file");
+    $object = json_decode($result);
+    $database = new Database();
+    $database->set_one_presence(
+      new ResultPresenceList(
+        rowid: 0,
+        matricula: $presence_item->matricula,
+        timestamp: new \DateTime(),
+        singularity: 1,
+        state: 201
+      )
+    );
+  }
+  catch (\Exception $erro)
   {
     new Template(
-      titulo: "Houve um erro!",
+      titulo: "Presença não registrada!",
       estilo: "",
       conteudo: <<<HTML
-        <p class="text-danger">Houve um erro ao receber o arquivo.</p>
+        <p class="text-danger">{$erro->getMessage()}</p>
         <p>Sua presença <strong>não</strong> foi registrada!</p>
         <p><a href="./">Voltar</a></p>
       HTML
       );
     http_response_code(400);
-    exit();
-  }
-  $presence_item = new RequestPresenceList($_POST["matricula"], $_FILES['camera_button']['tmp_name']);
-  $presence_item->is_valid();
-  $conteudo = "";
-  if (!empty($presence_item->erros_validacao))
-  {
-    foreach ($presence_item->erros_validacao as $erro)
-    {
-      $conteudo .= <<<HTML
-        <p class="text-danger">$erro</p>
-        HTML;
-      }
-      new Template(
-        titulo: "Registro inválido!",
-        estilo: "",
-        conteudo: $conteudo .= <<<HTML
-        <p>Sua presença <strong>não</strong> foi registrada!</p>
-        <p><a href="./">Voltar</a></p>
-      HTML
-      );
-    http_response_code(400);
-    exit();
-  }
-  $database = new Database();
-  $target_file = "/data/" . $presence_item->matricula . ".jpg";
-  // Se o arquivo não existir, cadastrar usuário.
-  if(!file_exists($target_file))
-  {
-    try
-    {
-      move_uploaded_file($presence_item->fotografia, $target_file);
-      $database->set_one_presence(
-        new ResultPresenceList(
-          rowid: 0,
-          matricula: $presence_item->matricula,
-          timestamp: new \DateTime(),
-          singularity: 1,
-          state: 201
-        )
-      );
-      new Template(
-        titulo: "Cadastro efetuado!",
-        estilo: "",
-        conteudo: <<<HTML
-          <p>Cadastro efetuado com sucesso!</p>
-          <p><a href="./">Voltar</a></p>
-        HTML
-        );
-      exit();
-    }
-    catch (\Exception $erro)
-    {
-      new Template(
-        titulo: "Algo deu errado!",
-        estilo: "",
-        conteudo: <<<HTML
-          <p class="text-danger">Houve um erro ao receber o arquivo.</p>
-          <p class="text-danger">{$erro->getMessage()}</p>
-          <p>Sua presença <strong>não</strong> foi registrada!</p>
-          <p><a href="./">Voltar</a></p>
-        HTML
-        );
-      exit();
-    }
   }
 ?>
